8ac4db9271b18725b9aa18d4934596ed061854ea
samuelea <batonenbois@gmail.com>
2025-11-04 15:57:50 -0500

    added connection and interrupt logic

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+
+	connectionUrl := "amqp://guest:guest@localhost:5672/"
+	connection, err := amqp.Dial(connectionUrl)
+
+	if err != nil {
+		log.Fatalf("Failed to connect to RabbitMQ: %v", err)
+	}
+
+	defer connection.Close()
+
+	fmt.Println("Connection successful!")
+
+	sigs := make(chan os.Signal, 1)
+
+	signal.Notify(sigs, syscall.SIGINT)
+
+	done := make(chan bool, 1)
+
+	go func() {
+		sig := <- sigs
+		fmt.Printf("\n Received signal: %v", sig)
+
+		done <- true
+	}()
+
+	<-done
+	fmt.Println("\n Application exited gracefully")
 }
